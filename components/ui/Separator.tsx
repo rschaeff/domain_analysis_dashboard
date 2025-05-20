c9f41c739a3d3f7757612f8640a53abc5548b6e8
R. Dustin Schaeffer <ZoroasterD@gmail.com>
2025-05-20 11:40:43 -0700

    Update Separator.tsx

--- components/ui/Separator.tsx
+++ components/ui/Separator.tsx
@@ -1,30 +1,26 @@
 "use client"
 
-import * as React from "react"
-import * as SeparatorPrimitive from "@radix-ui/react-separator"
+import React from "react"
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
->(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
+interface SeparatorProps {
+  className?: string
+  orientation?: "horizontal" | "vertical"
+}
+
+export function Separator({
+  className,
+  orientation = "horizontal",
+  ...props
+}: SeparatorProps & React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
       className={cn(
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 bg-gray-200",
+        orientation === "horizontal" ? "h-[1px] w-full my-2" : "h-full w-[1px] mx-2",
         className
       )}
       {...props}
     />
   )
-)
-Separator.displayName = SeparatorPrimitive.Root.displayName
-
-export { Separator }
+}
